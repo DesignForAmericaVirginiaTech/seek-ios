048ecaebc5900290cfbf19f19caa6b1b052c9121
Joe Bruzek <jbruzek@vt.edu>
2015-10-28 18:03:33 -0400

    Added Profile tab

---
+++ Seek/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Seek
+//
+//  Created by Joe Bruzek on 10/28/15.
+//  Copyright (c) 2015 Joe Bruzek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
